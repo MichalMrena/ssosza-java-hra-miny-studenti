df3f8315052671fc8ef63968bba4c3061cd776c3
Michal Mrena <mrena.michal@gmail.com>
2023-10-18 10:58:21 +0200

    2023-10-18

---
+++ src/michal/miny/Policko.java
@@ -0,0 +1,54 @@
+package michal.miny;
+
+public class Policko {
+
+    private boolean jeOdkryte;
+    private boolean jeOznacene;
+    private boolean maMinu;
+    private int pocetMinVOkoli;
+
+    public Policko(boolean maMinu, int pocetMinVOkoli) {
+        this.maMinu = maMinu;
+        this.pocetMinVOkoli = pocetMinVOkoli;
+        this.jeOdkryte = false;
+        this.jeOznacene = false;
+    }
+
+    public void odkrySa() {
+        this.jeOdkryte = true;
+    }
+
+    public void oznacSa() {
+        this.jeOznacene = true;
+    }
+
+    public void odznacSa() {
+        this.jeOznacene = false;
+    }
+
+    public boolean jeOdkryte() {
+        return this.jeOdkryte;
+    }
+
+    public boolean jeOznacene() {
+        return this.jeOznacene;
+    }
+
+    public boolean maMinu() {
+        return this.maMinu;
+    }
+
+    public int getPocetMinVOkoli() {
+        return this.pocetMinVOkoli;
+    }
+}
+
+
+
+
+
+
+
+
+
+
