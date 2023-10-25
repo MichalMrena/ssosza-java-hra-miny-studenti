9124648adba388903253546e787cc684d2077387
Michal Mrena <mrena.michal@gmail.com>
2023-10-25 10:59:11 +0200

    2023-10-25

---
+++ src/michal/miny/HraMiny.java
@@ -0,0 +1,41 @@
+package michal.miny;
+
+import java.util.ArrayList;
+
+/**
+ * Reprezentuje hracie pole.
+ * @author Michal
+ * @version 1.0.0
+ */
+public class HraMiny {
+
+    private Policko[][] policka;
+
+    public HraMiny(int pocetRiadkov, int pocetStlpcov, ArrayList<Pozicia> miny) {
+        this.policka = new Policko[pocetRiadkov][pocetStlpcov];
+        for (int riadok = 0; riadok < pocetRiadkov; riadok++) {
+            for (int stlpec = 0; stlpec < pocetStlpcov; stlpec++) {
+
+                boolean jeTamMina = this.obsahujeMinu(riadok, stlpec, miny);
+                this.policka[riadok][stlpec] = new Policko(jeTamMina, -1);
+            }
+        }
+    }
+
+    public boolean obsahujeMinu(int riadok, int stlpec, ArrayList<Pozicia> miny) {
+        for (Pozicia pozicia : miny) {
+            if (pozicia.jeRovnaka(riadok, stlpec)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
---
+++ src/michal/miny/Pozicia.java
@@ -0,0 +1,28 @@
+package michal.miny;
+
+public class Pozicia {
+    private int riadok;
+    private int stlpec;
+
+    public Pozicia(int riadok, int stlpec) {
+        this.riadok = riadok;
+        this.stlpec = stlpec;
+    }
+
+    public int getRiadok() {
+        return this.riadok;
+    }
+
+    public int getStlpec() {
+        return this.stlpec;
+    }
+
+    public boolean jeRovnaka(int riadok, int stlpec) {
+        return this.riadok == riadok && this.stlpec == stlpec;
+    }
+}
+
+
+
+
+
